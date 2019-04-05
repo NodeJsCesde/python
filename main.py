3a84a40540ea4360e7a46b3f4302499188872815
mauricio vargas romero <firebasecesde@gmail.com>
2019-04-04 20:28:23 -0500

    agregar
    se agrega ejmeplos de condicionales en python

---
+++ main.py
@@ -0,0 +1,62 @@
+# print("hola mundo")
+
+# input() entreda de datos 
+# int() para convertir cadena en entero
+
+# x = float(input("ingrese dato Uno: ")) # convierte los numeros en string 
+# #print(x)
+# y = float(input("ingrese dato Dos: ")) # convierte los numeros en string
+
+# print(x+y)
+
+# a = int(input("ingrese dato Uno: ")) # convierte los numeros en entero 
+#print(x)
+# b = int(input("ingrese dato Dos: ")) # convierte los numeros en entero
+# sacar la potencia es con doble **
+# suma +
+# resta -
+# multi *
+# divi /
+# potencia **
+# modulo %
+
+# boleanos
+# a = True
+# b = False
+# print(a)
+# print(b)
+
+# # potencia 
+# print(10**2)
+
+# # modulo
+# print(10%2)
+
+# if elif else
+
+# a = 5
+
+# if a < 4:
+#     print("hola")
+#     print("mundo")
+# elif a<5:
+#     print("es menor que 5")
+# else:
+#     print("No es menor")
+
+# # print("curso")
+
+a = 3
+b = 4
+
+if a == 3 or b == 4:
+    print("es correcto")
+
+
+# //// while
+# a = 7
+# while a == 7:
+#     print("estoy en el while")
+#     a = int(input("ingrese el nuevo valor de a:  "))
+#     if a != 7:
+#         break
